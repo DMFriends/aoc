dfd4c7e59ae92f13b32b58628e24565946f29a72
DMFriends <miretsky.daniel44@gmail.com>
2026-03-10 09:10:53 -0400

    Create Day10Part2.java

---
+++ src/main/java/_2021/day10/Day10Part2.java
@@ -0,0 +1,22 @@
+package _2021.day10;
+
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+
+public class Day10Part2
+{
+	public static void main(String[] args)
+	{
+		//try (Scanner input = new Scanner(new File("./src/main/java/_2025/day01/input.txt")))
+		try (Scanner input = new Scanner(new File("./src/main/java/_2025/day01/input_test.txt")))
+		{
+			
+		}
+		catch (FileNotFoundException ex)
+		{
+			System.out.println("Error! File not found!");
+		}
+	}
+}
